a4690609153961c71824554ede827e5760e23091
Md Rashidul Azam (Nishan) <arosh019@gmail.com>
2026-04-28 17:59:41 +0600

    fixed server and client error

--- server/db.js
+++ server/db.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const DB_PASS = process.env.DB_PASS;
-// const DB_URL = `mongodb+srv://btiahwpweb:${DB_PASS}@cluster0.9nknrlk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const DB_URL = `mongodb+srv://root:yqJFsPmX39u*mhc@cluster0.algbwlv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0`;
+const DB_URL = `mongodb+srv://btiahwpweb:${DB_PASS}@cluster0.9nknrlk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const DB_URL = `mongodb+srv://root:yqJFsPmX39u*mhc@cluster0.algbwlv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0`;
 
 let db;
 const client = new MongoClient(DB_URL, {
